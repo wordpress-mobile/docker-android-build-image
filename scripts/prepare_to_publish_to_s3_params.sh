02db9ae7aea96f767be97dcc17db677d37e41480
Oguz Kocer <oguz.kocer@automattic.com>
2021-07-17 04:28:21 -0400

    Use Buildkite parameters not CircleCI

--- scripts/prepare_to_publish_to_s3_params.sh
+++ scripts/prepare_to_publish_to_s3_params.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 function prepare_to_publish_to_s3_params() {
-    PARAMS=(--sha1=$CIRCLE_SHA1)
-    [[ -n "$CIRCLE_TAG" ]] && PARAMS+=(--tag-name=$CIRCLE_TAG)
-    [[ -n "$CIRCLE_BRANCH" ]] && PARAMS+=(--branch-name=$CIRCLE_BRANCH)
-    [[ -n "$CIRCLE_PULL_REQUEST" ]] && PARAMS+=(--pull-request-url=$CIRCLE_PULL_REQUEST)
+    PARAMS=(--sha1=$BUILDKITE_COMMIT)
+    [[ -n "$BUILDKITE_TAG" ]] && PARAMS+=(--tag-name=$BUILDKITE_TAG)
+    [[ -n "$BUILDKITE_BRANCH" ]] && PARAMS+=(--branch-name=$BUILDKITE_BRANCH)
+    [[ -n "$BUILDKITE_PULL_REQUEST" ]] && PARAMS+=(--pull-request-number=$BUILDKITE_PULL_REQUEST)
     echo "$PARAMS"
 }
