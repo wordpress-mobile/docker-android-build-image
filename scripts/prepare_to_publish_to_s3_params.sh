dcd06afd732e27059eded8fb9def966ff7a65ac3
Oguz Kocer <oguz.kocer@automattic.com>
2021-07-19 13:44:31 -0400

    Fix: Export prepare_to_publish_to_s3_params function

--- scripts/prepare_to_publish_to_s3_params.sh
+++ scripts/prepare_to_publish_to_s3_params.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export function prepare_to_publish_to_s3_params() {
+function prepare_to_publish_to_s3_params() {
     PARAMS=(--sha1=$BUILDKITE_COMMIT)
     [[ -n "$BUILDKITE_TAG" ]] && PARAMS+=(--tag-name=$BUILDKITE_TAG)
     [[ -n "$BUILDKITE_BRANCH" ]] && PARAMS+=(--branch-name=$BUILDKITE_BRANCH)
@@ -8,3 +8,4 @@ export function prepare_to_publish_to_s3_params() {
     echo "$PARAMS"
 }
 
+export -f prepare_to_publish_to_s3_params
