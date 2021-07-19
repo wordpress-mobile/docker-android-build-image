57fcef38518fa881b60326baac53473b5a48c27d
Oguz Kocer <oguz.kocer@automattic.com>
2021-07-19 12:50:09 -0400

    Export prepare_to_publish_to_s3_params function

--- scripts/prepare_to_publish_to_s3_params.sh
+++ scripts/prepare_to_publish_to_s3_params.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-function prepare_to_publish_to_s3_params() {
+export function prepare_to_publish_to_s3_params() {
     PARAMS=(--sha1=$BUILDKITE_COMMIT)
     [[ -n "$BUILDKITE_TAG" ]] && PARAMS+=(--tag-name=$BUILDKITE_TAG)
     [[ -n "$BUILDKITE_BRANCH" ]] && PARAMS+=(--branch-name=$BUILDKITE_BRANCH)
     [[ -n "$BUILDKITE_PULL_REQUEST" ]] && PARAMS+=(--pull-request-number=$BUILDKITE_PULL_REQUEST)
     echo "$PARAMS"
 }
+
